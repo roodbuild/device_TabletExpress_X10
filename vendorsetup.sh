67a2be61935e1ad024d3a2a6b0baf0ac75c4c8c2
roodbuild <neporood@gmail.com>
2017-01-04 18:30:05 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_X10-userdebug
-add_lunch_combo cm_X10-user
-add_lunch_combo cm_X10-eng
+add_lunch_combo X10-userdebug
+add_lunch_combo X10-user
+add_lunch_combo X10-eng
